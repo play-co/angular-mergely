e99bf53747024ab8932105ca0a929366cc033787
Chris Harding <chris@weeby.co>
2015-03-26 17:50:36 -0700

    Added gulp-ng-annotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
+var ngAnnotate = require('gulp-ng-annotate');
 
 var paths = {
   stylus: { src: 'src/*.styl', dist: 'dist' },
@@ -15,6 +16,7 @@ gulp.task('stylus', [], function() {
 
 gulp.task('js', [], function() {
   return gulp.src(paths.js.src)
+    .pipe(ngAnnotate())
     .pipe(gulp.dest(paths.js.dist));
 });
 
@@ -25,3 +27,4 @@ gulp.task('watch', ['dist'], function() {
   gulp.watch(paths.js.src, ['js']);
   gulp.watch(paths.stylus.src, ['stylus']);
 });
+
